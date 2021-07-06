db3dfb4d2b2aeb0c691002347b3740f562c37924
jayhan1109 <jungho1109@gmail.com>
2021-07-06 15:28:08 -0700

    Profile CollectionView Hedaer

--- IInstagram/Controller/Tabs/MainTabController.swift
+++ IInstagram/Controller/Tabs/MainTabController.swift
@@ -38,8 +38,8 @@ class MainTabController: UITabBarController {
     private func configureViewControllers(){
         view.backgroundColor = .white
         
-        let layout = UICollectionViewFlowLayout()
-        let feed = templateNavigationController(unselectedImage: #imageLiteral(resourceName: "home_unselected"), selectedImage: #imageLiteral(resourceName: "home_selected"), rootViewController: FeedController(collectionViewLayout: layout))
+        let feedLayout = UICollectionViewFlowLayout()
+        let feed = templateNavigationController(unselectedImage: #imageLiteral(resourceName: "home_unselected"), selectedImage: #imageLiteral(resourceName: "home_selected"), rootViewController: FeedController(collectionViewLayout: feedLayout))
         
         let search = templateNavigationController(unselectedImage: #imageLiteral(resourceName: "search_unselected"), selectedImage: #imageLiteral(resourceName: "search_selected"), rootViewController: SearchController())
         
@@ -47,7 +47,8 @@ class MainTabController: UITabBarController {
         
         let notification = templateNavigationController(unselectedImage: #imageLiteral(resourceName: "like_unselected"), selectedImage: #imageLiteral(resourceName: "like_selected"), rootViewController: NotificationController())
         
-        let profile = templateNavigationController(unselectedImage: #imageLiteral(resourceName: "profile_unselected"), selectedImage: #imageLiteral(resourceName: "profile_selected"), rootViewController: ProfileController())
+        let profileLayout = UICollectionViewFlowLayout()
+        let profile = templateNavigationController(unselectedImage: #imageLiteral(resourceName: "profile_unselected"), selectedImage: #imageLiteral(resourceName: "profile_selected"), rootViewController: ProfileController(collectionViewLayout: profileLayout))
         
         viewControllers = [feed, search, imageSelector, notification, profile]
         
--- IInstagram/View/Profile/ProfileHeader.swift
+++ IInstagram/View/Profile/ProfileHeader.swift
@@ -11,15 +11,132 @@ class ProfileHeader: UICollectionReusableView {
     
     // MARK: - Properties
     
+    private let profileImageView: UIImageView = {
+        let iv = UIImageView()
+        iv.image = #imageLiteral(resourceName: "ana")
+        iv.contentMode = .scaleAspectFill
+        iv.clipsToBounds = true
+        return iv
+    }()
+    
+    private let nameLabel : UILabel = {
+        let label = UILabel()
+        label.text = "Ana Lee"
+        label.font = UIFont.boldSystemFont(ofSize: 14)
+        return label
+    }()
+    
+    private lazy var editProfileFollowButton: UIButton = {
+        let btn = UIButton(type: .system)
+        btn.setTitle("Edit Profile", for: .normal)
+        btn.layer.cornerRadius = 3
+        btn.layer.borderColor = UIColor.lightGray.cgColor
+        btn.layer.borderWidth = 0.5
+        btn.titleLabel?.font = .boldSystemFont(ofSize: 14)
+        btn.setTitleColor(.black, for: .normal)
+        btn.addTarget(self, action: #selector(handlerEditProfileFollowTapped), for: .touchUpInside)
+        return btn
+    }()
+    
+    private lazy var postsLabel: UILabel = {
+        let label = UILabel()
+        label.numberOfLines = 0
+        label.textAlignment = .center
+        label.attributedStatText(value: 5, label: "posts")
+        return label
+    }()
+    
+    private lazy var followersLabel: UILabel = {
+        let label = UILabel()
+        label.numberOfLines = 0
+        label.textAlignment = .center
+        label.attributedStatText(value: 2, label: "followers")
+        return label
+    }()
+    
+    private lazy var followingLabel: UILabel = {
+        let label = UILabel()
+        label.numberOfLines = 0
+        label.textAlignment = .center
+        label.attributedStatText(value: 1, label: "following")
+        return label
+    }()
+    
+    private let gridButton: UIButton = {
+        let btn = UIButton(type: .system)
+        btn.setImage(#imageLiteral(resourceName: "grid"), for: .normal)
+        btn.tintColor = .link
+        return btn
+    }()
+    
+    private let listButton: UIButton = {
+        let btn = UIButton(type: .system)
+        btn.setImage(#imageLiteral(resourceName: "list"), for: .normal)
+        btn.tintColor = .lightGray
+        return btn
+    }()
+    
+    private let bookmarkButton: UIButton = {
+        let btn = UIButton(type: .system)
+        btn.setImage(#imageLiteral(resourceName: "ribbon"), for: .normal)
+        btn.tintColor = .lightGray
+        return btn
+    }()
+    
     // MARK: - Lifecycle
     
     override init(frame: CGRect) {
         super.init(frame: frame)
         
-        backgroundColor = .systemPink
+        backgroundColor = .white
+        
+        addSubview(profileImageView)
+        profileImageView.anchor(top: topAnchor, left: leftAnchor, paddingTop: 16, paddingLeft: 12)
+        profileImageView.setDimensions(height: 80, width: 80)
+        profileImageView.layer.cornerRadius = 80 / 2
+        
+        addSubview(nameLabel)
+        nameLabel.anchor(top:profileImageView.bottomAnchor, paddingTop: 12)
+        nameLabel.centerX(view: profileImageView)
+        
+        addSubview(editProfileFollowButton)
+        editProfileFollowButton.anchor(top: nameLabel.bottomAnchor, left: leftAnchor, right: rightAnchor, paddingTop: 16, paddingLeft: 24, paddingRight: 24)
+        
+        let stack = UIStackView(arrangedSubviews: [postsLabel, followersLabel,followingLabel])
+        stack.distribution = .fillEqually
+        stack.axis = .horizontal
+            
+        addSubview(stack)
+        stack.centerY(view: profileImageView)
+        stack.anchor(left: profileImageView.rightAnchor, right: rightAnchor, paddingLeft: 12, paddingRight: 12, height: 50)
+        
+        let topDivider = UIView()
+        topDivider.backgroundColor = .lightGray
+        
+        let bottomDivider = UIView()
+        bottomDivider.backgroundColor = .lightGray
+        
+        let buttonStack = UIStackView(arrangedSubviews: [gridButton, listButton, bookmarkButton])
+        buttonStack.distribution = .fillEqually
+        
+        addSubview(buttonStack)
+        addSubview(topDivider)
+        addSubview(bottomDivider)
+        
+        buttonStack.anchor(left: leftAnchor, bottom: bottomAnchor, right: rightAnchor, height: 50)
+        
+        topDivider.anchor(top: buttonStack.topAnchor, left: leftAnchor, right: rightAnchor, height: 0.5)
+        bottomDivider.anchor(top: buttonStack.bottomAnchor, left: leftAnchor, right: rightAnchor, height: 0.5)
     }
     
     required init?(coder: NSCoder) {
         fatalError("init(coder:) has not been implemented")
     }
+    
+    // MARK: - Actions
+    
+    @objc func handlerEditProfileFollowTapped(){
+        print("edit profile tapped")
+    }
+    
 }
