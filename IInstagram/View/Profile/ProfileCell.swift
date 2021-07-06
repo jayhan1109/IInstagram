0ea692d357d87bf49ca62634ed4daaeed8fb8b27
jayhan1109 <jungho1109@gmail.com>
2021-07-06 15:27:46 -0700

    Initial Profile UICollectionCell UI

--- IInstagram/View/Profile/ProfileCell.swift
+++ IInstagram/View/Profile/ProfileCell.swift
@@ -11,12 +11,22 @@ class ProfileCell: UICollectionViewCell {
     
     // MARK: - Properties
     
+    private let postImageView: UIImageView = {
+        let iv = UIImageView()
+        iv.image = #imageLiteral(resourceName: "baptist")
+        iv.contentMode = .scaleAspectFill
+        iv.clipsToBounds = true
+        return iv
+    }()
+    
     // MARK: - Lifecycle
     
     override init(frame: CGRect) {
         super.init(frame: frame)
         
         backgroundColor = .lightGray
+        addSubview(postImageView)
+        postImageView.fillSuperView()
     }
     
     required init?(coder: NSCoder) {
